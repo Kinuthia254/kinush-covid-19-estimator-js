787a4d68ab4a45b34c8603433c0638fff8d1909a
Kinuthia254 <muchenekevin@gmail.com>
2020-04-10 12:00:36 +0300

    commit 2

--- src/estimator.js
+++ src/estimator.js
@@ -1,13 +1,10 @@
-
 function doubleSet(timeToElapse) {
   return Math.ceil(2 ** (timeToElapse / 3));
 }
 
-function weeks(timeToElapse){
-
-  
+function weeks(timeToElapse) {
   let week = Math.floor(timeToElapse / 7);
-  return Math.ceil((3 ** 3) * week )
+  return Math.ceil((3 ** 3) * week);
 }
 
 const covid19ImpactEstimator = (data) => {
@@ -28,14 +25,13 @@ const covid19ImpactEstimator = (data) => {
   severeImpact.currentlyInfected = data.reportedCases * 50;
 
   const powerDays = doubleSet(data.timeToElapse);
-  const powerWeek = (weeks(data.timeToElapse));
+  const powerWeek = weeks(data.timeToElapse);
 
-  
   impact.infectionsByRequestedTime = impact.currentlyInfected * powerDays;
   impact.infectionsByRequestedTime = impact.currentlyInfected * powerWeek;
 
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * powerDays;
-  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * powerWeek;
+  severeImpact.infectionsByRequestedTime =severeImpact.currentlyInfected * powerWeek;
 
   return output;
 };
