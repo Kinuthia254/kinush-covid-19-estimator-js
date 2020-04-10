f2686c40779679ed85eea7264ac61a6cc828c9e0
Kinuthia254 <muchenekevin@gmail.com>
2020-04-10 11:15:30 +0300

    starting on challeng1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,31 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const output = {};
+
+  output.data = data;
+
+  const impact = {};
+
+  output.impact = impact;
+
+  const severeImpact = {};
+
+  output.severeImpact = severeImpact;
+
+  impact.currentlyInfected = data.reportedCases * 10;
+
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+
+  const power = doubleSet(data.timeToElapse);
+  impact.infectionsByRequestedTime = impact.currentlyInfected * power;
+
+  severeImpact.infectionsByRequestedTime =
+    severeImpact.currentlyInfected * power;
+
+  return output;
+};
+
+function doubleSet(timeToElapse) {
+  return Math.ceil(Math.pow(2, timeToElapse / 3));
+}
 
 export default covid19ImpactEstimator;
